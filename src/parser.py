754df262f19d2dd5fb518ad94981ba6976104d84
baoobab <v.xxl.90@mail.ru>
2025-01-12 23:49:17 +0300

    separated parsers from /parse route handler

--- src/parser.py
+++ src/parser.py
@@ -1,58 +1,25 @@
 ﻿from flask import Flask, request, jsonify
-from seleniumbase import Driver
-import os
+from parser_workers.parser_example_domain_bs4 import parse as p1
+from parser_workers.parser_example_domain import parse as p2
 from dotenv import load_dotenv
-
+from typing import Tuple
+import os
 load_dotenv()
 
-# Глобальный экземпляр драйвера
-driver = Driver()
 app = Flask(__name__)
+parsers = [p1, p2] # Доступные парсеры (TODO: глобальный скоуп такое, потом вынести)
 
-def init_driver():
-    global driver
-    options = {"uc": True, "headless": True}
-    driver = Driver(**options)
-    driver.uc_open_with_reconnect(os.getenv("PARSE_URL"))
+def parse_blocklist(domain) -> str:
+    global parsers
 
-    print("driver inited")
-
-
-def parse_blocklist(domain):
-    try:
-        driver.refresh()
-
-        input_field = driver.find_element("#Check_URL")
-        input_field.clear()  # Очищаем поле
-        input_field.send_keys(domain)
-        driver.click("#SendFormBut")
-
-        results = None
-
-        try:
-            driver.wait_for_element(".clean .middle", timeout=10)
-            results = driver.get_text(".clean .middle")
-        except Exception:
-            driver.wait_for_element(".blocklist", timeout=5)
-            results = driver.get_text(".blocklist")
-
-        results = results.split(' ')
-        result = results[0]
-        if (
-                result.startswith("С")
-        ):  # когда results = ['Состояние:', 'заблокирован\nIP-адрес:'] => блокнут
-            results = "Blocked"
-        elif (len(results) > 2 and results[2].startswith("в")
-              ):  # когда results = ['Искомый', 'ресурс', 'включен'] => блокнут
-            results = "Blocked"
-        else:  # когда results = ['Искомый', 'ресурс', 'не', 'найден'] => доступен
-            results = "Available"
-
-        return results if results else "No results"
-
-    except Exception as e:
-        return f"Parsing err: {str(e)}"
-    
+    errors_string = "" # стркоа для возврата ошибок, конкатится к результату
+    for p_func in parsers:
+        p_result, p_error = p_func(domain=domain)
+        if p_result:
+            return f"Blocked{errors_string}" # Одного вхождения достаточно - возвращаем
+        if p_error:
+            errors_string += f" error: {p_error}"
+    return f"Available{errors_string}"
 
 @app.route("/parse", methods=["POST"])
 def parse_route():
@@ -71,6 +38,5 @@ def handle_exception(e):
     return f"Err on parser: {str(e)}", 500
 
 if __name__ == '__main__':
-    init_driver()
     app.run(port=os.getenv("PARSER_APP_PORT", 5004))  # Порт для парсера
 
---
+++ src/parser_workers/parser_example_domain_bs4.py
@@ -0,0 +1,45 @@
+﻿from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from requests import get
+from typing import Tuple
+import os
+load_dotenv()
+
+# Указываем доступность для экспорта - только функцию parse
+__all__ = ['parse']
+
+# Глобальные настройки
+parsing_url = os.getenv("PARSE_URL_2")
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    }
+
+def parse(domain) -> Tuple[bool, str]:
+    global headers, parsing_url
+
+    if not parsing_url:
+        return False, f"no url to parse {parsing_url}"
+    
+    try:
+        response = get(f"{parsing_url}{domain}", headers=headers)
+                
+        if response.status_code != 200:
+            print("Не удалось получить данные", response)
+            return False, "cannot get"
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        results = soup.select('.search-info .item p')
+        noted_count = 0
+        result = False
+
+        if len(results) > 1:
+            noted_count = int(str(results[1]).replace("<p>", "").replace("</p>", ""))
+        if noted_count > 0:
+            result = True
+
+        return result, ""
+    except Exception as e:
+        print(f"Произошла ошибка при парсинге: {str(e)}")
+        return False, f"err: {e}"
+    
